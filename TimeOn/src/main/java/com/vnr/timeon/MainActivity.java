98b482edd926ec18ee6e9245706622e551c953ed
Verose <verose@live.com>
2014-04-12 15:36:31 +0300

    UI and Activity tests
    Added layout with EditText and Button to direct to another Activity.

--- TimeOn/src/main/java/com/vnr/timeon/MainActivity.java
+++ TimeOn/src/main/java/com/vnr/timeon/MainActivity.java
@@ -1,15 +1,29 @@
 package com.vnr.timeon;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+    public final static String EXTRA_NAME_MESSAGE = "com.example.TimeOn.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the ok button */
+    public void okMessage(View view) {
+        // send this to DisplayMessageActivity
+        Intent intent = new Intent(this, DisplayMessageOKActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+
+        // add key-value pairs
+        intent.putExtra(EXTRA_NAME_MESSAGE, message);
+        startActivity(intent);
     }
 }
