b96a44bf14995e6caa1931fae0a2f6f403b7eb38
Tranquility2 <roy@moore.co.il>
2014-04-16 12:58:51 +0300

    Simple Time display & update

--- TimeOn/src/main/java/com/vnr/timeon/MainActivity.java
+++ TimeOn/src/main/java/com/vnr/timeon/MainActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends Activity {
     public final static String EXTRA_NAME_MESSAGE = "com.example.TimeOn.MESSAGE";
@@ -25,5 +29,17 @@ public class MainActivity extends Activity {
         // add key-value pairs
         intent.putExtra(EXTRA_NAME_MESSAGE, message);
         startActivity(intent);
+    }
+
+    /** Called when the user clicks the update_time button **/
+    public void updateTimeText(View view) {
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current time => "+c.getTime());
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = df.format(c.getTime());
+
+        TextView txtView = (TextView) findViewById(R.id.Time_now);
+        txtView.setText(formattedDate);
     }
 }
