5035ec8fc028de6920985575b5cd84650abae5db
Tranquility2 <roy@moore.co.il>
2014-05-08 22:35:07 +0300

    Added About
    Update UI
    (Refactor some of the stuff)

---
+++ TimeOn/src/main/java/com/vnr/timeon/DisplayAbout.java
@@ -0,0 +1,55 @@
+package com.vnr.timeon;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class DisplayAbout extends ActionBarActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_about);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_NAME_MESSAGE);
+
+        // Create the text view
+        textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("Version =  " + message);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.activity_display_about, null);
+        // Find the LinearLayout element
+        LinearLayout ll = (LinearLayout) v.findViewById(R.id.linearLayoutMain);
+        // Add elements the LinearLayout
+        ll.addView(textView);
+        // Display the view
+        setContentView(v);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- TimeOn/src/main/java/com/vnr/timeon/MainActivity.java
+++ TimeOn/src/main/java/com/vnr/timeon/MainActivity.java
@@ -3,8 +3,10 @@ package com.vnr.timeon;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -19,16 +21,27 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Called when the user clicks the ok button **/
-    public void okMessage(View view) {
-        // send this to DisplayMessageActivity
-        Intent intent = new Intent(this, DisplayMessageTestActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_name_message);
-        String message = editText.getText().toString();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
 
-        // add key-value pairs
-        intent.putExtra(EXTRA_NAME_MESSAGE, message);
-        startActivity(intent);
+    /** Called when the user click on the about menu **/
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                // send this to DisplayAboutActivity
+                Intent intent = new Intent(this, DisplayAbout.class);
+                // get the version
+                String value = Integer.toString(android.os.Build.VERSION.SDK_INT);
+                // add key-value pairs
+                intent.putExtra(EXTRA_NAME_MESSAGE, value);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     /** Called when the user clicks the update_time button **/
